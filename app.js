1e68472a6281789fe15264d8ff7aa17e06521751
Anmol Benipal <124711038+anmolbenipal@users.noreply.github.com>
2024-06-25 15:17:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,11 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production"){
 app.use(express.static("client"));
-const path = require("path);
-app.get("*",(req,res)=>{
-res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-})
-                     }
+}
 
 
 
